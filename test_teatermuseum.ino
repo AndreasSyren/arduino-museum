c493119c79d95a329a0224a3740d3297b1491321
Andreas Syren <me@andreassyren.se>
2016-05-25 23:42:34 +0200

    Fixed connection with browser

--- test_teatermuseum.ino
+++ test_teatermuseum.ino
@@ -1,19 +1,27 @@
+
+int pinArray[] = {8, 9, 10, 11, 12, 13};
+int count = 0;
+  
 void setup()
 {
   Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  for (count=0;count<6;count++) {
+      pinMode(pinArray[count], OUTPUT);
+  }
 }
-
 void loop()
 {
   char in;
   if (Serial.available()) {
     in = Serial.read();
-    if (in == '!') {
+    if (in == '2') {
       digitalWrite(13, HIGH);
+    } else if (in == '3') {
+      digitalWrite(12, HIGH);
     } else {
-      digitalWrite(13, LOW);
-    }
+      digitalWrite(12,LOW);
+      digitalWrite(13,LOW);
+      }
   }
 }
   

0dff70649dc7a938cf9c2d7149ac519299089792
Andreas Syren <me@andreassyren.se>
2016-05-27 12:59:11 +0200

    Added servo

--- test_teatermuseum.ino
+++ test_teatermuseum.ino
@@ -1,12 +1,20 @@
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
 
 int pinArray[] = {8, 9, 10, 11, 12, 13};
 int count = 0;
-  
+
+
 void setup()
 {
   Serial.begin(9600);
-  for (count=0;count<6;count++) {
-      pinMode(pinArray[count], OUTPUT);
+
+  myservo.attach(9);
+
+  for (count = 0; count < 6; count++) {
+    pinMode(pinArray[count], OUTPUT);
   }
 }
 void loop()
@@ -18,10 +26,19 @@ void loop()
       digitalWrite(13, HIGH);
     } else if (in == '3') {
       digitalWrite(12, HIGH);
-    } else {
-      digitalWrite(12,LOW);
-      digitalWrite(13,LOW);
+    } else if (in == '4') {
+      for (pos = 0; pos <= 180; pos += 1) {
+        myservo.write(pos);
+        delay(15);
       }
+      for (pos = 180; pos >= 0; pos -= 1) {
+        myservo.write(pos);
+        delay(15);
+      }
+    } else {
+      digitalWrite(12, LOW);
+      digitalWrite(13, LOW);
+    }
   }
 }
-  
+
